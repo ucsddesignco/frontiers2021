5a0ca68d439169ac159054f3b44157404608db38
Adam Syed <syedadamemirputra@gmail.com>
2021-05-12 05:23:39 -0700

    Responsive Details

--- src/components/Details.js
+++ src/components/Details.js
@@ -5,26 +5,41 @@ function Details() {
   return (
     <div>
       <div className="details-container">
-        <Grid container justify="center" spacing={5}>
-          <Grid item sm={10} md={5}>
+        <Grid
+          className="details-container2"
+          container
+          justify="center"
+          spacing={5}
+        >
+          <Grid item xs={10} md={5}>
             <div class="details-info">
               <h2>Who can participate?</h2>
-              <p>Registration is open to the public, but we will give priority to UCSD students.</p>
+              <p>
+                Registration is open to the public, but we will give priority to
+                UCSD students.
+              </p>
             </div>
           </Grid>
-          <Grid item sm={10} md={5}>
+          <Grid item xs={10} md={5}>
             <div class="details-info">
               <h2>How big can my team be?</h2>
-              <p>Teams can be a min of 2 and a max of 4. Teams will not be assigned, so feel free to make your own or join one on Design Co’s Discord channel.</p>
+              <p>
+                Teams can be a min of 2 and a max of 4. Teams will not be
+                assigned, so feel free to make your own or join one on Design
+                Co’s Discord channel.
+              </p>
             </div>
           </Grid>
-          <Grid item sm={10} md={5}>
+          <Grid item xs={10} md={5}>
             <div class="details-info">
               <h2>How long is Design Frontiers?</h2>
-              <p>Design Frontiers is 4 days long and will be held on May 20 - May 23, 2021.</p>
+              <p>
+                Design Frontiers is 4 days long and will be held on May 20 - May
+                23, 2021.
+              </p>
             </div>
           </Grid>
-          <Grid item sm={10} md={5}>
+          <Grid item xs={10} md={5}>
             <div class="details-info">
               <h2>When is the deadline to register?</h2>
               <p>Wednesday, May 19 at 11:59PM PDT</p>
@@ -33,7 +48,7 @@ function Details() {
         </Grid>
       </div>
     </div>
-  ); 
+  );
 }
 
 export default Details;
