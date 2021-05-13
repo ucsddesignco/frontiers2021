e5f77f30e4bf95a32e7a6a7165f1126dc7a3c7af
Adam Syed <syedadamemirputra@gmail.com>
2021-05-13 06:22:24 -0700

    Added meta data

--- src/App.js
+++ src/App.js
@@ -3,10 +3,15 @@ import Landing from "./components/Landing.js";
 import Brief from "./components/Brief";
 import Timeline from "./components/Timeline";
 import Details from "./components/Details";
+import { Helmet } from "react-helmet";
 
 function App() {
   return (
     <div>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Frontiers 2021: Design Co</title>
+      </Helmet>
       <div className="App">
         <Landing />
         <Details />
