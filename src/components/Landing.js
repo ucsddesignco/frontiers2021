3eac369e9809e22d98a7af4e799bb92635bf3f57
Shawn Kim <sjk002@ucsd.edu>
2021-05-12 20:43:24 -0700

    updated landing

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -2,23 +2,61 @@ import React from "react";
 import { Grid } from '@material-ui/core';
 import "./Landing.css";
 import Navbar from "./Navbar.js";
-import DesignCo from "../assets/images/dco-logo.svg";
-import Banner from "../assets/images/frontiers-banner.svg"
+import topleft from "../assets/images/top-left.svg";
+import topright from "../assets/images/top-right.svg";
+import bottomleft from "../assets/images/bottom-left.svg";
+import bottomcenter from "../assets/images/bottom-center.svg";
+import bottomright from "../assets/images/bottom-right.svg";
+
 
 function Landing() {
   return (
-    <div className="row">
+    <div>
       <div className="Landing">
         <div className="logo"/>
         <Navbar></Navbar>
+        <div className="nav-menu"></div>
       </div>
 
-      <div className="landing-container">
-        <div className="banner"/>
+      <div className="row">
+        <Grid container alignItems="center" direction="row" spacing={1} className="top-container">
+          <Grid container item xs={8} sm={9} md={9}>
+           <div className="banner-topleft">
+             <p className="long-text">Design Frontiers 2021</p>
+             <p className="short-text">Design Frontiers</p>
+           </div> 
+            
+            
+          </Grid>
+          <Grid container item xs={4} sm={3} md={3} justify="flex-start">
+            <div className="banner-topright"></div>
+          </Grid>
+        </Grid>
+
+        
+
+        <Grid container alignItems="center" className="bottom-container">
+          <Grid container item xs={2} sm={2} md={2}>
+            <div className="banner-bottomleft" />
+          </Grid>
+          <Grid container item xs={1} sm={4} md={4}>
+            <div className="banner-bottomcenter" />
+          </Grid>
+          <Grid container item xs={9} sm={6} md={6}>
+            <div className="banner-bottomright" />
+          </Grid>  
+        </Grid>
+        
       </div> 
+
       <div className="row">
         <div className="landing-text">Design Frontiers is Design Co’s annual design sprint where teams of creative, collaborative individuals tackle real problems in the world and design for innovation.</div>
       </div>
+
+      <div className="hole">
+        <div className="button"></div>
+      </div>
+      
     
     </div>
   )
